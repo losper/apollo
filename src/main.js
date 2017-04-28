703812b211cb85a199631664f15fada81150085f
liuwenjun <liuwenjun@hangsheng.com.cn>
2017-04-28 16:02:09 +0800

    v2 version

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ var reshandler = require('./reshandler');
 var querystring = require('querystring');
 var url = require("url");
 var db=require('./db.js');
-
+var webhooks=require('./webhooks.js');
 db.setup("queue.db");
 
 
@@ -15,9 +15,9 @@ http.createServer(function (req, res) {
   
   req.on('end', function () {
     // 设置响应头部信息及编码
-	var params = url.parse(req.url, true).query;
-
+	
 	if(body==""){
+		var params = url.parse(req.url, true).query;
 		var ext=url.parse(req.url).pathname;
 		console.log(ext);
 		if(ext=="/log"){
@@ -26,27 +26,8 @@ http.createServer(function (req, res) {
 			reshandler.filter(req,res,ext);
 		}
 	}else{
-		console.log(params.X-gogs-Event);
-		res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
-		var json  = JSON.parse(body);
-		
-		switch(json.object_kind){
-			case "push":{
-				db.push(json.repository.git_http_url,json.commits[0].message,json.user_name,json.repository.name);
-				res.end();
-			}
-			break;
-			case "history":
-				db.getHistory(res);
-			break;
-			case "currentVersion":
-				db.getVersion(res);
-			break;
-			default:
-			res.end();
-			break;
-		}
+		webhooks.handle(req,res,body,db);
 	}
   });
-}).listen(80);
+}).listen(8008);
 
